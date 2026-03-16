502b552b3d7f33cd7ac6287d45e34e9ddafcee39
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2026-03-16 15:51:08 +0000

    fix: included correct directories in CMakeLists.txt
    feat: made a dummy program in main.c to test out the program

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include "vm.h"
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    printf("Welcome to the Virtual Machine!\n");
+
+    int program[] = {
+        OP_PUSH, 8,
+        OP_PUSH, 5,
+        OP_ADD,
+        OP_PRT,
+        OP_HLT
+    };
+
+    VM vm = {0};
+
+    vm.program = program;
+    vm.running = 1;
+
+    vm_run(&vm);
+
     return 0;
 }
